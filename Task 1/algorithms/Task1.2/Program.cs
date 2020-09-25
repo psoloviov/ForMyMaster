2def09b4d4db3450978800de0061c8774f78a3e3
psoloviov <pavlo.soloviov@nure.ua>
2020-09-25 21:35:24 +0300

    add task 1.2, fix gitignore

---
+++ Task 1/algorithms/Task1.2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Task1._2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                var min = Convert.ToInt32(Console.ReadLine());
+                var max = Convert.ToInt32(Console.ReadLine());
+                DisplayRange(min, max);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Wops! Something went wrong!");
+            }
+            Console.ReadKey();
+            
+        }
+
+        static void DisplayRange(int min, int max)
+        {
+            for (int i = min; i <= max; )
+            {
+                Console.Write($"{i} ");
+                i++;
+            }
+        }
+    }
+}
