0e8259cacde35b8bf4315a0a6adf90f6a032256d
psoloviov <pavlo.soloviov@nure.ua>
2020-09-25 16:50:50 +0300

    Update gitignore and delete program files

--- Task 1/algorithms/Task1.0/Program.cs
+++ Task 1/algorithms/Task1.0/Program.cs
@@ -7,43 +7,43 @@ namespace Task1._0
     {
         public static void Main(string[] args)
         {
-            int n = Convert.ToInt32(Console.ReadLine()); /*в шарпе все данные которые введены в консоль имеют
-                                                        имеют тип данных string, от того и вводимое число 
-                                                        сразу конвертируется в int */
+            int masLength = Convert.ToInt32(Console.ReadLine()); /* all input console information have string format.
+                                                        i convert string to int */
 
-            int[] mas = new int[n]; //создаю масив с пользовательскими данными
-            GenerateMassive(n, mas); //заполняю массив случайными числами
-            Display(n, mas); //вывожу массив с данными на экран
+            int[] mas = new int[masLength]; //create array
+            GenerateMassive(masLength, mas); //fill the array
+            Display(masLength, mas); //outputting an array with data
 
-            int Sum = new int(); // для вывода среднего числа
-            for (int i = 0; i < n; i++)
+            int Sum = new int(); //middle output
+            for (int i = 0; i < masLength; i++)
             {
                 Sum += mas[i];
             }
 
             Console.WriteLine(
-                $"Middle v1: {Sum / 2}, Middle v2: {mas[n / 2 - 1]}"); //не было уточнения какое именно "среднее"
+                $"Middle v1: {Sum / 2}, Middle v2: {mas[masLength / 2 - 1]}"); //two ways to find the middle
 
-            Console.WriteLine($"Minimum: {mas.Min()}, Maximum: {mas.Max()}"); //перавый способ вывода, ниже второй
-            //Console.WriteLine("Minimum: {0}, Maximum: {1}", mas.Min(), mas.Max());
+            Console.WriteLine($"Minimum: {mas.Min()}, Maximum: {mas.Max()}"); //First way
+            //Console.WriteLine("Minimum: {0}, Maximum: {1}", mas.Min(), mas.Max()); The second way is presented below.
 
-            Console.ReadKey(); //чтобы не закрывалась консоль
+            Console.ReadKey(); //the console did not close
         }
 
-        static void GenerateMassive(int n, int[] mas)
+        static void GenerateMassive(int masLength, int[] mas)
         {
+            
             Random random = new Random(); //переменная имеющая случайное число
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i < masLength; i++)
             {
                 mas[i] = random.Next(-20, 20); //присваиваю рандом число от -20 до 20
             }
         }
 
-        static void Display(int n, int[] mas)
+        static void Display(int masLength, int[] mas)
         {
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i < masLength; i++)
             {
-                Console.WriteLine($"Element {i + 1} = {mas[i]}"); //вывел числа массива
+                Console.WriteLine($"Element {i + 1} = {mas[i]}"); //array output
             }
         }
     }
