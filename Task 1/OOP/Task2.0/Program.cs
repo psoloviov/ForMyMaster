c1ee699850a13712f836948271005f38d445e3bd
psoloviov <pavlo.soloviov@nure.ua>
2020-09-28 14:31:43 +0300

    Start task 2.0

---
+++ Task 1/OOP/Task2.0/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Task2._0
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Vector vector1 = new Vector(2,5,4);
+            Vector vector2 = new Vector(4, -1, -6);
+            Console.WriteLine(vector1.ScalarProduct(vector2));
+            Console.WriteLine(vector1.Cos(vector2));
+        }
+    }
+}
---
+++ Task 1/OOP/Task2.0/Vector.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Task2._0
+{
+    public class Vector
+    {
+        private readonly double x;
+        private readonly double y;
+        private readonly double z;
+
+        public Vector(double x, double y, double z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public double Length()
+        {
+            return Math.Sqrt(x * x + y * y + z * z);
+        }
+
+        public double ScalarProduct(Vector vector)
+        {
+            return x * vector.x + y * vector.y + z * vector.z;
+        }
+
+        public Vector CrossProduct(Vector vector)
+        {
+            return new Vector(
+                y * vector.z - z * vector.y,
+                z * vector.x - x * vector.z,
+                x * vector.y - y * vector.x);
+        }
+
+        public double Cos(Vector vector)
+        {
+            return ScalarProduct(vector) / (Length() * vector.Length());
+        }
+
+        public Vector Add(Vector vector)
+        {
+            return new Vector(
+                x + vector.x,
+                y + vector.y,
+                z + vector.z
+            );
+        }
+
+        public Vector Subtract(Vector vector)
+        {
+            return new Vector(
+                x - vector.x,
+                y - vector.y,
+                z - vector.z);
+        }
+
+        public static Vector[] Generate(int n)
+        {
+            Vector[] vectors = new Vector[n];
+            var rand = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                vectors[i] = new Vector(rand.Next(), rand.Next(), rand.Next());
+            }
+
+            return vectors;
+        }
+
+        public String String()
+        {
+            return $"Vector[ x = {x}, y = {y}, z = {z} ]";
+        }
+    }
+}
