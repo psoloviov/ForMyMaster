750e99b24aaaa0ee84c7f241e9d4596abc33f9ad
psoloviov <pavlo.soloviov@nure.ua>
2020-09-29 20:44:04 +0300

    Add task 2.1

---
+++ Task 1/OOP/Task2.1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task2._1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] numbers = new[] {1, 2, 3};
+            int[] weight = new[] {1, 2, 10};
+            RandomArray rand = new RandomArray(numbers, weight);
+
+            
+
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(rand.getRandom());
+            }
+        }
+    }
+}
---
+++ Task 1/OOP/Task2.1/RandomArray.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Task2._1
+{
+    public class RandomArray
+    {
+        private int[] valuesRange;
+
+        public RandomArray(int[] numbers, int[] weight)
+        {
+            int sum = 0;    
+            foreach (var i in weight)
+            {
+                sum += i;
+            }
+
+            valuesRange = new int[sum];
+            int index = 0;
+            
+            for (int i = 0; i < weight.Length; i++)
+            {
+                for (int j = 0; j < weight[i]; j++)
+                {
+                    valuesRange[index] = numbers[i];
+                    index++;
+                }
+            }
+        }
+
+        public int getRandom()
+        {
+            Random rand = new Random();
+            int randomNumber = rand.Next() % (valuesRange.Length - 1);
+            return valuesRange[randomNumber];
+        }
+    }
+}
