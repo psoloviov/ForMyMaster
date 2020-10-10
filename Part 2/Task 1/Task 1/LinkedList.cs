7207c46ada763bf1c7e9b2de59925f13357d0581
psoloviov <pavlo.soloviov@nure.ua>
2020-10-10 20:12:47 +0300

    sketch
    try to solve task 1

---
+++ Part 2/Task 1/Task 1/LinkedList.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Task_1
+{
+    public class LinkedList
+    {
+        private int[] array;
+        private int index;
+        // Код ниже не работает :(
+        // public void Input()
+        // {
+        //     var inputNumber = new int();
+        //     do
+        //     {
+        //         Console.WriteLine("Введите число: ");
+        //         var str = Console.ReadLine();
+        //         bool result = Int32.TryParse(str, out inputNumber);
+        //         if (result)
+        //         {
+        //             if (inputNumber != 0)
+        //             {
+        //                 index++;
+        //                 int[] tmp = new int[index];
+        //                 array = tmp;
+        //                 array[index] = inputNumber;
+        //             }
+        //             else
+        //             {
+        //                 break;
+        //             }
+        //         }
+        //         else
+        //         {
+        //             Console.WriteLine("Вы ввели некорректное число! Попробуйте снова.");
+        //             Console.WriteLine("Или введите \"0\" Для выхода.");
+        //         }
+        //         
+        //     } while (inputNumber != 0);
+        // }
+    }
+}
---
+++ Part 2/Task 1/Task 1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace Task_1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var s = new LinkedList();
+            s.Input();
+            
+            // foreach(var n in numbers)
+            //     Console.WriteLine(n);
+        }
+    }
+}
