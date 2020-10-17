b5e26d0d52f4153d6f547b30ef52a9a7a8d63a4a
psoloviov <pavlo.soloviov@nure.ua>
2020-10-17 20:00:44 +0300

    Complete Parse

---
+++ Part 2/Task 1/Task 2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace Task_2
+{
+    internal class Program
+    {
+        struct str
+        {
+            public string Name;
+            public string Value;
+        }
+        public static void Main(string[] args)
+        {
+            string path = Console.ReadLine();
+            if (File.Exists(path))
+            {
+                var list=new List<str>();
+                foreach (string s in File.ReadAllLines(path))
+                    list.Add(new str { Name = s.Split(' ')[0], Value = s.Split(' ')[1] });
+            }
+            else
+            {
+                Console.WriteLine("Такого файла не существует!");
+            }
+        }
+    }
+}
