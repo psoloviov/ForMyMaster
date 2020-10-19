0d4a82ac561b30e690553b2bbc7716ad8742ffde
psoloviov <pavlo.soloviov@nure.ua>
2020-10-19 20:49:36 +0300

    add OOP style
    All logic was remove to file "UkraineRegions"

--- Part 2/Task 1/Task 3/Program.cs
+++ Part 2/Task 1/Task 3/Program.cs
@@ -6,96 +6,10 @@ namespace Task_3
 {
     internal class Program
     {
-        struct str
-        {
-            public string Region;
-            public int Population;
-        }
-
         public static void Main(string[] args)
         {
-            var list = new List<str>();
-            FillList(ref list);
-
-            var searchRegion = true;
-
-            do
-            {
-                Console.Write("Введите номер области чтобы посмотреть её население: ");
-                var tmp = Console.ReadLine();
-
-                if (tmp != null)
-                {
-                    switch (tmp.ToLower())
-                    {
-                        case "выход":
-                            searchRegion = false;
-                            break;
-                        case "список":
-                            ShowAvailableRegions(ref list);
-                            break;
-                        default:
-                            RegionInformation(tmp, ref list);
-                            break;
-                    }
-                }
-            } while (searchRegion);
-        }
-
-        private static void FillList(ref List<str> list)
-        {
-            var index = 1;
-            list.Add(new str() {Region = "Винницкая область", Population = 842_842});
-            list.Add(new str() {Region = "Волынская область", Population = 845_621});
-            list.Add(new str() {Region = "Днепропетровская область", Population = 108_842});
-            list.Add(new str() {Region = "Донецкая область", Population = 132_846});
-            list.Add(new str() {Region = "Житомирская область", Population = 2_846_548});
-            list.Add(new str() {Region = "Закарпатская область", Population = 84_542});
-            list.Add(new str() {Region = "Запорожская область", Population = 5_845_842});
-            list.Add(new str() {Region = "Ивано-Франковская область", Population = 9_954_001});
-            list.Add(new str() {Region = "Киевская область", Population = 32_158});
-            list.Add(new str() {Region = "Кировоградская область", Population = 894_512});
-            list.Add(new str() {Region = "Луганская область", Population = 108});
-            list.Add(new str() {Region = "Львовская область", Population = 186_128});
-            list.Add(new str() {Region = "Николаевская область", Population = 48_842});
-            list.Add(new str() {Region = "Одесская область", Population = 942_518});
-            list.Add(new str() {Region = "Полтавская область", Population = 320_184});
-            list.Add(new str() {Region = "Ровненская область", Population = 75_121});
-            list.Add(new str() {Region = "Сумская область", Population = 890_020});
-            list.Add(new str() {Region = "Тернопольская область", Population = 132_021});
-            list.Add(new str() {Region = "Харьковская область", Population = 731_016});
-            list.Add(new str() {Region = "Херсонская область", Population = 21_308});
-            list.Add(new str() {Region = "Хмельницкая область", Population = 123_123});
-            list.Add(new str() {Region = "Черкасская область", Population = 605_018});
-            list.Add(new str() {Region = "Черниговская область", Population = 843_015});
-            list.Add(new str() {Region = "Черновицкая область", Population = 48_010});
-        }
-
-        private static void ShowAvailableRegions(ref List<str> list)
-        {
-            foreach (var item in list)
-            {
-                Console.WriteLine($"{item.Region}");
-            }
-
-            Console.WriteLine();
-        }
-
-        private static void RegionInformation(string region, ref List<str> list)
-        {
-            var noneOutput = true;
-            foreach (var item in list.Where(item =>
-                string.Equals(region, item.Region, StringComparison.CurrentCultureIgnoreCase) ||
-                string.Equals(region, item.Region.Replace(" область", null),
-                    StringComparison.CurrentCultureIgnoreCase)))
-            {
-                Console.WriteLine($"{item.Region} насчитывает в себе {item.Population} людей.");
-                noneOutput = false;
-            }
-
-            if (!noneOutput) return;
-            Console.WriteLine("Совпадений не найдено! Убедитесь в правильности ввода!");
-            Console.WriteLine("Или введите \"выход\" для выхода и \"список\" для просмотра доступных областей");
+            var s = new UkraineRegions();
+            s.InteractionWithRegions();
         }
     }
 }
---
+++ Part 2/Task 1/Task 3/UkraineRegions.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task_3
+{
+    public class UkraineRegions
+    {
+        private string _region;
+        private int _population;
+        private static readonly List<UkraineRegions> List = new List<UkraineRegions>();
+
+        private UkraineRegions(string region, int population)
+        {
+            _region = region;
+            _population = population;
+        }
+
+        public UkraineRegions()
+        {
+            FillList();
+        }
+
+        public void InteractionWithRegions()
+        {
+            var searchRegion = true;
+            do
+            {
+                Console.Write("Введите номер области чтобы посмотреть её население: ");
+                var currentRegion = Console.ReadLine();
+                if (currentRegion == null) return;
+                switch (currentRegion.ToLower())
+                {
+                    case "выход":
+                        searchRegion = false;
+                        break;
+                    case "список":
+                        ShowAvailableRegions();
+                        break;
+                    default:
+                        RegionInformation(currentRegion);
+                        break;
+                }
+            } while(searchRegion);
+        }
+
+        private void FillList()
+        {
+            List.Add(new UkraineRegions(_region = "Винницкая область", _population = 842_842));
+            List.Add(new UkraineRegions(_region = "Волынская область", _population = 845_621));
+            List.Add(new UkraineRegions(_region = "Днепропетровская область", _population = 108_842));
+            List.Add(new UkraineRegions(_region = "Донецкая область", _population = 132_846));
+            List.Add(new UkraineRegions(_region = "Житомирская область", _population = 2_846_548));
+            List.Add(new UkraineRegions(_region = "Закарпатская область", _population = 84_542));
+            List.Add(new UkraineRegions(_region = "Запорожская область", _population = 5_845_842));
+            List.Add(new UkraineRegions(_region = "Ивано-Франковская область", _population = 9_954_001));
+            List.Add(new UkraineRegions(_region = "Киевская область", _population = 32_158));
+            List.Add(new UkraineRegions(_region = "Кировоградская область", _population = 894_512));
+            List.Add(new UkraineRegions(_region = "Луганская область", _population = 108));
+            List.Add(new UkraineRegions(_region = "Львовская область", _population = 186_128));
+            List.Add(new UkraineRegions(_region = "Николаевская область", _population = 48_842));
+            List.Add(new UkraineRegions(_region = "Одесская область", _population = 942_518));
+            List.Add(new UkraineRegions(_region = "Полтавская область", _population = 320_184));
+            List.Add(new UkraineRegions(_region = "Ровненская область", _population = 75_121));
+            List.Add(new UkraineRegions(_region = "Сумская область", _population = 890_020));
+            List.Add(new UkraineRegions(_region = "Тернопольская область", _population = 132_021));
+            List.Add(new UkraineRegions(_region = "Харьковская область", _population = 731_016));
+            List.Add(new UkraineRegions(_region = "Херсонская область", _population = 21_308));
+            List.Add(new UkraineRegions(_region = "Хмельницкая область", _population = 123_123));
+            List.Add(new UkraineRegions(_region = "Черкасская область", _population = 605_018));
+            List.Add(new UkraineRegions(_region = "Черниговская область", _population = 843_015));
+            List.Add(new UkraineRegions(_region = "Черновицкая область", _population = 48_010));
+        }
+
+        private static void ShowAvailableRegions()
+        {
+            foreach (var item in List)
+            {
+                Console.WriteLine($"{item._region}");
+            }
+
+            Console.WriteLine();
+        }
+
+        private static void RegionInformation(string region)
+        {
+            var noneOutput = true;
+            foreach (var item in List.Where(item =>
+                item._region != null &&
+                (string.Equals(region, item._region, StringComparison.CurrentCultureIgnoreCase) ||
+                 string.Equals(region, item._region.Replace(" область", null),
+                     StringComparison.CurrentCultureIgnoreCase))))
+            {
+                Console.WriteLine($"{item._region} насчитывает в себе {item._population} людей.");
+                noneOutput = false;
+            }
+
+            if (!noneOutput) return;
+            Console.WriteLine("Совпадений не найдено! Убедитесь в правильности ввода!");
+            Console.WriteLine("Или введите \"выход\" для выхода и \"список\" для просмотра доступных областей");
+        }
+    }
+}
