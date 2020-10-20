bce99a0a910fee55fbd9d3c8dbe362896b2321b6
psoloviov <pavlo.soloviov@nure.ua>
2020-10-20 15:38:05 +0300

    Add OOP & Change logic

---
+++ Part 2/Task 1/Task 1/Numbers.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task_1
+{
+    public class Numbers
+    {
+        private static string _str = null;
+        private static readonly List<int> List = new List<int>();
+
+        public Numbers()
+        {
+        }
+
+        public void EnterNumbers()
+        {
+            Console.Write("Введите положительные числа: ");
+            var validNumber = true;
+            do
+            {
+                var key = Console.ReadKey().Key;
+
+                switch (key)
+                {
+                    case ConsoleKey.NumPad1:
+                    case ConsoleKey.D1:
+                        _str += 1;
+                        break;
+
+                    case ConsoleKey.NumPad2:
+                    case ConsoleKey.D2:
+                        _str += 2;
+                        break;
+
+                    case ConsoleKey.NumPad3:
+                    case ConsoleKey.D3:
+                        _str += 3;
+                        break;
+
+                    case ConsoleKey.NumPad4:
+                    case ConsoleKey.D4:
+                        _str += 4;
+                        break;
+
+                    case ConsoleKey.NumPad5:
+                    case ConsoleKey.D5:
+                        _str += 5;
+                        break;
+
+                    case ConsoleKey.NumPad6:
+                    case ConsoleKey.D6:
+                        _str += 6;
+                        break;
+
+                    case ConsoleKey.NumPad7:
+                    case ConsoleKey.D7:
+                        _str += 7;
+                        break;
+
+                    case ConsoleKey.NumPad8:
+                    case ConsoleKey.D8:
+                        _str += 8;
+                        break;
+
+                    case ConsoleKey.NumPad9:
+                    case ConsoleKey.D9:
+                        _str += 9;
+                        break;
+
+                    case ConsoleKey.NumPad0:
+                    case ConsoleKey.D0:
+                        if (_str == null)
+                            validNumber = false;
+                        else
+                            _str += 0;
+                        break;
+
+                    case ConsoleKey.Spacebar:
+                        if (_str != null)
+                        {
+                            var tmp = int.Parse(_str);
+                            if (tmp > 0)
+                            {
+                                List.Add(tmp);
+                                _str = null;
+                            }
+                            else
+                            {
+                                validNumber = false;
+                            }
+                        }
+
+                        break;
+
+                    case ConsoleKey.OemMinus:
+                        if (_str == null) _str += "-";
+                        else
+                        {
+                            Error();
+                            _str = null;
+                        }
+
+                        break;
+
+                    default:
+                        Error();
+                        Console.Write("Попробуйте снова:");
+                        _str = null;
+                        break;
+                }
+            } while (validNumber);
+
+            Console.WriteLine();
+        }
+
+        public void Output()
+        {
+            var noneOutput = true;
+            do
+            {
+                Console.Write("Введите элемент по которому делить: ");
+                if (int.TryParse(Console.ReadLine(), out var number))
+                    if (List.Count > number && number >= 0)
+                    {
+                        Console.Write("Меньше выбранного числа: ");
+                        foreach (var item in List.Where(item => item <= List[number]))
+                            Console.Write($"{item} ");
+                        Console.WriteLine();
+                        Console.Write("Больше выбранного числа: ");
+                        foreach (var item in List.Where(item => item > List[number]))
+                            Console.Write($"{item} ");
+                        Console.WriteLine();
+                        noneOutput = false;
+                    }
+                    else
+                    {
+                        Error();
+                    }
+                else
+                {
+                    Error();
+                }
+            } while (noneOutput);
+        }
+
+        private static void Error()
+        {
+            Console.WriteLine("Error!");
+        }
+    }
+}
--- Part 2/Task 1/Task 1/Program.cs
+++ Part 2/Task 1/Task 1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Task_1
 {
@@ -7,128 +8,9 @@ namespace Task_1
     {
         public static void Main(string[] args)
         {
-            var list = new LinkedList<int>();
-            EnterNumbers(ref list);
-            
-            Console.Write("Введите число, которым хотите разделить массив: ");
-            var userInput = int.Parse(Console.ReadLine());
-            Output(ref list, userInput);
-            Console.Write("Вывод чисел: ");
-            foreach (var item in list)
-                Console.Write($"{item} ");
-        }
-
-        public static void Output(ref LinkedList<int> list, int userInput)
-        {
-            Console.Write("Числа больше: ");
-            foreach (var item in list)
-            {
-                if (item <= userInput)
-                    Console.Write($"{item} ");
-            }
-
-            Console.WriteLine();
-            Console.Write("Числа меньше: ");
-            foreach (var item in list)
-            {
-                if (item > userInput)
-                    Console.Write($"{item} ");
-            }
-
-            Console.WriteLine();
-        }
-
-        public static void EnterNumbers(ref LinkedList<int> list)
-        {
-            Console.Write("Введите положительные числа: ");
-            string str = null;
-            bool validNumber = true;
-            do
-            {
-                var key = Console.ReadKey().Key;
-
-                switch (key)
-                {
-                    case ConsoleKey.NumPad1:
-                    case ConsoleKey.D1:
-                        str += 1;
-                        break;
-
-                    case ConsoleKey.NumPad2:
-                    case ConsoleKey.D2:
-                        str += 2;
-                        break;
-
-                    case ConsoleKey.NumPad3:
-                    case ConsoleKey.D3:
-                        str += 3;
-                        break;
-
-                    case ConsoleKey.NumPad4:
-                    case ConsoleKey.D4:
-                        str += 4;
-                        break;
-
-                    case ConsoleKey.NumPad5:
-                    case ConsoleKey.D5:
-                        str += 5;
-                        break;
-
-                    case ConsoleKey.NumPad6:
-                    case ConsoleKey.D6:
-                        str += 6;
-                        break;
-
-                    case ConsoleKey.NumPad7:
-                    case ConsoleKey.D7:
-                        str += 7;
-                        break;
-
-                    case ConsoleKey.NumPad8:
-                    case ConsoleKey.D8:
-                        str += 8;
-                        break;
-
-                    case ConsoleKey.NumPad9:
-                    case ConsoleKey.D9:
-                        str += 9;
-                        break;
-
-                    case ConsoleKey.NumPad0:
-                    case ConsoleKey.D0:
-                        str += 0;
-                        break;
-
-                    case ConsoleKey.Spacebar:
-                        if (str != null)
-                        {
-                            int tmp = Int32.Parse(str);
-                            if (tmp > 0)
-                            {
-                                list.AddLast(tmp);
-                                str = null;
-                            }
-                            else
-                            {
-                                validNumber = false;
-                            }
-                        }
-
-                        break;
-
-                    case ConsoleKey.OemMinus:
-                        str += '-';
-                        break;
-
-                    default:
-                        Console.WriteLine(" Упс, кажется вы ввели не число!");
-                        Console.Write("Попробуйте снова:");
-                        str = null;
-                        break;
-                }
-            } while (validNumber);
-
-            Console.WriteLine();
+            var numbers = new Numbers();
+            numbers.EnterNumbers();
+            numbers.Output();
         }
     }
 }
