8cb904885f1255ffba968bc536f2a3d7850d0275
psoloviov <pavlo.soloviov@nure.ua>
2020-11-10 21:54:57 +0200

    Create Log File

--- Part 2/Task 4 v2/FileOutput.cs
+++ Part 2/Task 4 v2/FileOutput.cs
@@ -1,24 +1,51 @@
 ﻿using System;
+using System.IO;
 
 namespace Task_4_v2
 {
     public class FileOutput
     {
-        private const string Path = ".";
+        private const string Path = @"log.txt";
 
-        static void CreateFile()
+        public void CreateFile()
         {
-            
+            File.Create(Path).Close();
         }
 
-        static void PrintTurn()
+        public void PrintTurn(Player player1, Player player2)
         {
-            
+            using (var stream = new StreamWriter(Path, true))
+            {
+                if (player1.Queue.Count != 0 && player2.Queue.Count != 0)
+                {
+                    var card1 = player1.Queue.Peek().Value
+                                + " " + player1.Queue.Peek().Suit;
+                    var card2 = player2.Queue.Peek().Value
+                                + " " + player2.Queue.Peek().Suit;
+                    
+                    stream.WriteLine($"Turn {Game.TurnCount} ");
+                    stream.WriteLine($"Cards: {card1} and {card2} ");
+                }
+                else
+                {
+                    stream.WriteLine($"# # # Game Finish! # # #");
+                }
+            }
         }
 
-        static void PrintWinner(Guid playerId)
+        public static void PrintPickUp(Player player)
         {
-            
+            using (var stream = new StreamWriter(Path, true))
+            {
+                stream.WriteLine($"Cards takes player {player.Name}");
+                stream.WriteLine();
+            }
+        }
+
+        public void PrintWinner(Player player)
+        {
+            using (var stream = new StreamWriter(Path, true))
+                stream.WriteLine($"Player {player.Name} won! \n");
         }
     }
 }
